a193317e80515898a127513c880d2fe8e665b21a
Moses Okode <okodekings76@gmail.com>
2024-11-27 18:23:43 +0300

    clean state

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -19,32 +19,35 @@ const memeData = {
              "height": 437,
              "box_count": 2
           },
-          {
-            "id": "61579",
+         {
+            "id": "61580",
             "name": "One Does Not Simply",
-            "url": "https://i.imgflip.com/1bij.jpg",
+            "url": "https://project-static-assets.s3.amazonaws.com/APISpreadsheets/APIMemes/WhoIsJason.jpeg",
             "width": 568,
             "height": 335,
             "box_count": 2,
             "captions": 1750 // estimated number of all-time captions of this meme, rounded
+           
+
          },
          {
-            "id": "61579",
-            "name": "One Does Not Simply",
-            "url": "https://i.imgflip.com/1bij.jpg",
-            "width": 568,
-            "height": 335,
-            "box_count": 2,
-            "captions": 1750 // estimated number of all-time captions of this meme, rounded
+            "id": "101471",
+            "name": "Ancient Aliens",
+            "url": " https://th.bing.com/th/id/OIP.iMSJLm0WSLT7-HwepEsFTwHaJH?w=500&h=616&rs=1&pid=ImgDetMain",
+            "width": 500,
+            "height": 447,
+            "box_count": 3
          },
          {
-            "id": "61579",
-            "name": "One Does Not Simply",
-            "url": "https://i.imgflip.com/123abc.jpg",
-            "page_url": "https://imgflip.com/i/123abc",
-            "template_id": 89370399,
-            "texts": ["you can't get caught to sing a conversation", "if you don't have any money"]
+            "id": "101472",
+            "name": "Ancient Aliens",
+            "url": " https://cdn0.vox-cdn.com/thumbor/27qOtm3QTaPofS1QyoZKoUpOPNc=/0x11:500x292/1600x900/cdn0.vox-cdn.com/imported_assets/1682605/ZU0cbbr.png",
+            "width": 500,
+            "height": 447,
+            "box_count": 4
          },
+         
+        
 
          // probably a lot more memes here..
       ]
